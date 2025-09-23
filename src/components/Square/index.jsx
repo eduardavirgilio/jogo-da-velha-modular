e76e071a3f7e168a28bb18d744a3e8f26fbe82ab
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-09-23 11:24:18 -0300

    ATT: comentario no square

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -1,5 +1,5 @@
 
-// O Square é um componente  que representa um quadrado do tabuleiro do jogo da velha
+// O Square é um componente que representa um quadrado do tabuleiro do jogo da velha
 // O value vai passar o valor de cada quadrado, podendo ser X, O ou null se o quadrado estiver vazio
 // Essa função faz o valor aparecer dentro do btn que foi clicado
 
