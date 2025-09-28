5985818ba03bc61b53be68c4101a1245b3f74237
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-09-28 20:45:08 -0300

    ADD: comentarios explicando a função Game

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,24 +1,36 @@
+// função  que gerencia o estado do jogo da velha
+
 import { useState } from 'react';
 import Board from '../Board';
 
 export default function Game() {
+  // armazena o histórico de todas as configurações do tabuleiro
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  // rastreia qual movimento está sendo visualizado 
   const [currentMove, setCurrentMove] = useState(0);
+  // determina se é a vez do jogador X (movimentos pares)
   const xIsNext = currentMove % 2 === 0;
+  // configuração atual das casas do tabuleiro a partir do histórico
   const currentSquares = history[currentMove];
 
+  // função chamada pelo componente Board para atualizar o estado após um movimento
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+  // atualiza o histórico.
     setHistory(nextHistory);
+  // move o ponteiro para o novo último movimento
     setCurrentMove(nextHistory.length - 1);
   }
 
+  // função para "voltar no tempo", alterando o índice do movimento atual
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
+  // mapeia o array de histórico para criar uma lista de botões.
   const moves = history.map((squares, move) => {
     let description;
+  // define o texto do botão
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
@@ -31,6 +43,7 @@ export default function Game() {
     );
   });
 
+  // renderiza o componente
   return (
     <div className="game">
       <div className="game-board">
--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -1,7 +1,7 @@
 
-// O Square é um componente que representa um quadrado do tabuleiro do jogo da velha
-// O value vai passar o valor de cada quadrado, podendo ser X, O ou null se o quadrado estiver vazio
-// Essa função faz o valor aparecer dentro do btn que foi clicado
+// o square é um componente que representa um quadrado do tabuleiro do jogo da velha
+// o value vai passar o valor de cada quadrado, podendo ser X, O ou null se o quadrado estiver vazio
+// essa função faz o valor aparecer dentro do btn que foi clicado
 
 export default function Square({ value, onSquareClick }) {
   return (
